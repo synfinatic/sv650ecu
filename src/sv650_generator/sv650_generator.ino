21441f79c46917ffb6e7b5a57721b2093d4cb734
Aaron Turner <synfinatic@gmail.com>
2013-09-09 17:54:46 -0700

    add two Arduino tools I used to help develop the code in sv650_reader Neither are particularly pretty/clean, but they are good starting points for creating your own ECU emulator for other Suzuki serial based ECU's.

---
+++ src/sv650_generator/sv650_generator.ino
@@ -0,0 +1,170 @@
+/* 
+ * Copyright 2013 Aaron Turner
+ *
+ * Arduino based SV650 ECU TACH line emulator.  I wrote this to 
+ * make it easier to test my sv650 decoder code.  
+ */
+#include <SoftwareSerial.h>
+#include <ST6961.h>
+#define CS 10
+#define MOSI 11
+#define CLK 13
+
+#define RX 7
+#define TX 8                // ECU pin  
+#define BUTTON 4
+
+#define ECU_SPEED 7800
+#define SERIAL_SPEED 115200
+#define BLINK_LED 13 
+#define BLINK_MS 500
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are long's because the time, measured in miliseconds,
+// // will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
+// init SoftwareSerial for ECU communications.  Need to keep ecu global
+SoftwareSerial ecu(RX, TX);
+ST6961 LED(MOSI, CLK, CS);
+
+
+/*
+ *  Error codes to rotate through
+ */
+typedef struct _ECU_CODES {
+    byte bytes[8];
+} ECU_CODES;
+
+static ECU_CODES ecu_codes_table[] =
+{
+    { 0x5a , 0x10 , 0x0  , 0    , 0    , 0    , 0    , 0 }    , 
+    { 0x1c , 0xc0 , 0x0  , 0    , 0    , 0    , 0    , 0 }    , 
+    { 0x10 , 0xc0 , 0x0  , 0    , 0    , 0    , 0    , 0 }    , 
+    { 0x2c , 0xc8 , 0x0  , 0    , 0x40 , 0    , 0    , 0 }    , 
+    { 0x2c , 0xc8 , 0x80 , 0    , 0    , 0    , 0    , 0 }    , 
+    { 0x2c , 0xc8 , 0x80 , 0    , 0xa0 , 0    , 0    , 0 }    , 
+    { 0xff , 0xff , 0xff , 0xff , 0xff , 0xff , 0xff , 0xff }
+};
+
+// How long to show each error code
+static unsigned int code_times[] = 
+{
+    4000, 3000, 3000, 6000, 6000, 10000
+};
+
+
+// printf to the HW serial port
+void 
+serial_printf(char *fmt, ... ) {
+    char tmp[128]; // resulting string limited to 128 chars
+    va_list args;
+    va_start (args, fmt );
+    vsnprintf(tmp, 128, fmt, args);
+    va_end (args);
+    Serial.print(tmp);
+}
+
+/*
+ * Calculate the checksum
+ */
+byte
+gen_csum(unsigned int code) {
+    byte check_sum = 0;
+
+    // Calculate check sum byte  
+    for (int x = 0; x <= 6; x++) {
+        check_sum = check_sum + ecu_codes_table[code].bytes[x];
+    }
+
+    check_sum = 256 - check_sum;
+
+    return check_sum;
+}
+
+/*
+ *  Initialize everything
+ */
+void 
+setup() {
+    unsigned int i = 0;
+    ecu.begin(ECU_SPEED);
+    Serial.begin(SERIAL_SPEED);
+    pinMode(BUTTON, INPUT);
+
+    // calc checksums for all the messages
+    while (ecu_codes_table[i].bytes[0] != 0xff) {
+        serial_printf("calculating checksum for %u\n", i);
+        ecu_codes_table[i].bytes[7] = gen_csum(i);
+        serial_printf("OK\n");
+        i++;
+    }
+    serial_printf("Sending: %02x %02x %02x %02x %02x %02x %02x %02x\n", 
+        ecu_codes_table[0].bytes[0], 
+        ecu_codes_table[0].bytes[1], 
+        ecu_codes_table[0].bytes[2], 
+        ecu_codes_table[0].bytes[3], 
+        ecu_codes_table[0].bytes[4], 
+        ecu_codes_table[0].bytes[5], 
+        ecu_codes_table[0].bytes[6], 
+        ecu_codes_table[0].bytes[7]);
+}
+/*
+ *  Blink our status light
+ */
+void 
+blink() {
+    static int hilo = 0;
+    static unsigned long last = millis();
+
+    if (millis() - last >= BLINK_MS) {
+        hilo = ! hilo;
+        digitalWrite(BLINK_LED, hilo);
+        last = millis();
+    }
+}
+
+void 
+loop() {
+    int x;
+    static unsigned int ecu_code = 0;
+    static unsigned long message_ms = millis();
+    unsigned long now;
+
+    // Write data bytes to serial
+    for (x = 0; x <= 7; x++) {
+        ecu.write(ecu_codes_table[ecu_code].bytes[x]);
+        delay(10);
+    }
+
+    // Intermessage delay
+    delay(20);  // only use 20ms because we already slept 10ms earlier
+    blink();
+
+
+    // Do we go to the next message?
+    now = millis();
+    if (now >= message_ms + code_times[ecu_code]) {
+        message_ms = now;
+        // go to next code
+        if (ecu_codes_table[ecu_code+1].bytes[0] == 0xff) {
+            ecu_code = 0;
+        } else {
+            ecu_code ++;
+        }
+        serial_printf("Sending for %ums: %02x %02x %02x %02x %02x %02x %02x %02x\n", 
+            code_times[ecu_code],
+            ecu_codes_table[ecu_code].bytes[0], 
+            ecu_codes_table[ecu_code].bytes[1], 
+            ecu_codes_table[ecu_code].bytes[2], 
+            ecu_codes_table[ecu_code].bytes[3], 
+            ecu_codes_table[ecu_code].bytes[4], 
+            ecu_codes_table[ecu_code].bytes[5], 
+            ecu_codes_table[ecu_code].bytes[6], 
+            ecu_codes_table[ecu_code].bytes[7]);
+    }
+}
---
+++ src/sv650_temp/sv650_temp.ino
@@ -0,0 +1,148 @@
+/* 
+ * Copyright 2013 Aaron Turner
+ *
+ * Wrote this code to make it easy to built the temperature ADC map
+ * since the SV650 ECU has the ADC and it's up to the dash to convert
+ * that into degrees F/C.  I used this with the OEM dash to figure out
+ * what ADC values mapped to what temperatures.
+ *
+ */
+#include <SoftwareSerial.h>
+#include <ST6961.h>
+#define CS 10
+#define MOSI 11
+#define CLK 13
+
+#define RX 7
+#define TX 8                // ECU pin  
+#define UP_BUTTON 4
+#define DOWN_BUTTON 3
+
+#define ECU_SPEED 7800
+#define BLINK_LED 13 
+#define BLINK_MS 500
+int sbytes[8];     // 7 bytes for serial data
+
+// init SoftwareSerial for ECU communications.  Need to keep ecu global
+SoftwareSerial ecu(RX, TX);
+ST6961 LED(MOSI, CLK, CS);
+
+
+void 
+clear_buf()
+{
+    int j;
+    for (j = 0; j < 7; j++) {
+        sbytes[j] = 0;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.print("Starting\n");
+    LED.initDisplay();
+    ecu.begin(ECU_SPEED);
+    clear_buf();
+    calc_csum();
+    pinMode(UP_BUTTON, INPUT);
+    pinMode(DOWN_BUTTON, INPUT);
+}
+
+/*
+ * calculates the checksum of sbytes[]
+ */
+void  
+calc_csum() {
+    byte check_sum = 0;
+
+    // Calculate check sum byte  
+    for (int x = 0; x <= 6; x++) {
+        check_sum = check_sum + sbytes[x];
+    }
+
+    sbytes[7] = 256 - check_sum;
+}
+
+// printf to the HW serial port
+void 
+serial_printf(char *fmt, ... ) {
+    char tmp[128]; // resulting string limited to 128 chars
+    va_list args;
+    va_start (args, fmt );
+    vsnprintf(tmp, 128, fmt, args);
+    va_end (args);
+    Serial.print(tmp);
+}
+
+unsigned int 
+increment_temp() 
+{
+    unsigned int temp = 0;
+    temp = sbytes[0] << 2;
+    temp += (sbytes[1] & 0xc0) >> 6;
+
+    temp += 1;
+    sbytes[0] = temp >> 2;
+    sbytes[1] = temp << 6;
+
+    serial_printf("temp = %04u\n", temp);
+    calc_csum();
+    return temp;
+}
+
+unsigned int 
+decrement_temp() 
+{
+    unsigned int temp = 0;
+    temp = sbytes[0] << 2;
+    temp += (sbytes[1] & 0xc0) >> 6;
+
+    temp -= 1;
+
+    sbytes[0] = temp >> 2;
+    sbytes[1] = temp << 6;
+
+    serial_printf("temp = %04u\n", temp);
+    calc_csum();
+    return temp;
+}
+
+void 
+print_temp(unsigned int temp)
+{
+   int i = 0;
+   byte val;
+   byte led_digits[4] = { 0xc0, 0xc2, 0xc4, 0xc6 };
+   LED.sendNum((int)temp, 0);
+}
+
+void loop() 
+{
+    int x;
+    unsigned int temp;
+
+    // Write data bytes to serial
+    for (x = 0; x <= 6; x++) {
+        ecu.write(sbytes[x]);
+        delay(10);
+    }
+    ecu.write(sbytes[7]);
+
+    // Intermessage delay
+    delay(30);
+    if (digitalRead(UP_BUTTON)) {
+        temp = increment_temp();
+        print_temp(temp);
+        serial_printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", 
+                sbytes[0], sbytes[1], sbytes[2], sbytes[3], 
+                sbytes[4], sbytes[5], sbytes[6], sbytes[7]);
+   
+    } else if (digitalRead(DOWN_BUTTON)) {
+        temp = decrement_temp();
+        print_temp(temp);
+        serial_printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", 
+                sbytes[0], sbytes[1], sbytes[2], sbytes[3], 
+                sbytes[4], sbytes[5], sbytes[6], sbytes[7]);
+   
+    }
+}
